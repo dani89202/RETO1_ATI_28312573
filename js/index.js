918fab2691f481bd83871c242347971d843c4836
dani89202 <danielaa.carreoa@gmail.com>
2025-05-08 16:01:54 -0400

    listado de estudiantes dinámico usando javascript

--- js/index.js
+++ js/index.js
@@ -41,5 +41,39 @@ document.addEventListener("DOMContentLoaded", () => {
       .catch(error => {
         console.error("No se pudo cargar el JSON local:", error);
       });
+
+
+      // Cargar lista de estudiantes desde estudiantes.json
+    fetch("datos/index.json")
+        .then(response => {
+            if (!response.ok) throw new Error("Error cargando estudiantes.json");
+            return response.json();
+        })
+        .then(estudiantes => {
+            const lista = document.getElementById("lista-estudiantes");
+
+            estudiantes.forEach(est => {
+                const li = document.createElement("li");
+
+                // Creamos el enlace a perfil.html con el ci en el parametro
+                const link = document.createElement("a");
+                link.href = `perfil.html?ci=${est.ci}`;
+
+                const img = document.createElement("img");
+                img.src = `${est.imagen}`;
+                img.alt = `Foto de ${est.nombre}`;
+
+                const span = document.createElement("span");
+                span.textContent = est.nombre;
+                
+                link.appendChild(img)
+                li.appendChild(link);
+                li.appendChild(span);
+                lista.appendChild(li);
+            });
+        })
+        .catch(error => {
+            console.error("Error al cargar estudiantes:", error);
+        });
   });
   
